e2b7f954b8423acbb303e6b432ae1e4004c3b0de
kruger.joshua <109550490+krugerjoshua@users.noreply.github.com>
2025-02-04 10:03:57 +0200

    Corrected newVelocityKmH so correct values can be displayed in the console.

--- index.js
+++ index.js
@@ -13,29 +13,30 @@ const distanceKm = 0; // distance (km)
 const fuelKg = 5000; // remaining fuel (kg)
 const fuelBurnRateKgPerSecond = 0.5; // fuel burn rate (kg/s)
 
-const newDistanceKm = distanceKm + (velocityKmH * timeSeconds) //calcultes new distance
-const remainingFuel = fuelBurnRateKgPerSecond*timeSeconds //calculates remaining fuel
 
 // Pick up an error with how the function below is called and make it robust to such errors
-const velocityMs = velocityKmH * (1000 / 3600) // Unit conversion for proper calculation
+// Unit conversion for proper calculation
+const velocityMs = velocityKmH * (1000 / 3600) 
+
 function calcNewVel(velocityMs, accelerationMs2, timeSeconds) {
   if (velocityMs < 0 || accelerationMs2 < 0 ||  timeSeconds < 0) {
     console.log("Incorrect values entered, please try again with positive numbers.")
     return
   } else {
-    return velocityMs + (accelerationMs2 * timeSeconds)
+    return velocityMs + accelerationMs2 * timeSeconds
   }
 }
 
-const newVelocityMs = calcNewVel(velocityKmH, accelerationMs2, timeSeconds) //calculates new velocity based on acceleration.
-const newVelocityKmH = velocityMs * (1000 / 3600)
+//calculates new velocity based on acceleration.
+const newVelocityMs = calcNewVel(velocityMs, accelerationMs2, timeSeconds) 
+const newVelocityKmH = newVelocityMs * (3600 / 1000)
 
-console.log(`Corrected New Velocity: ${newVelocityKmH} km/h`);
-console.log(`Corrected New Distance: ${newDistanceKm} km`);
-console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
+//calcultes new distance
+const newDistanceKm = distanceKm + (velocityMs * timeSeconds + 0.5 * accelerationMs2 * timeSeconds ** 2) / 1000;
 
+// Fuel calculation, calculates remaining fuel
+const remainingFuel = fuelKg - fuelBurnRateKgPerSecond * timeSeconds
 
-
-
-
-
+console.log(`Corrected New Velocity: ${newVelocityKmH.toFixed(2)} km/h`);
+console.log(`Corrected New Distance: ${newDistanceKm.toFixed(2)} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel.toFixed(2)} kg`);
